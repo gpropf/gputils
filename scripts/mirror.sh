a2a6b6667ae0d3dba5af6a81cfc76859fe0789ac
Greg Propf <gpropf@gmail.com>
2025-11-05 12:53:38 -0800

    Added some more file lists to back up and some better logging.

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -16,6 +16,8 @@ TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
 files=("Writing" "backup-test" "Dropbox" "Projects")
 symlinks=("2025" "BUSINESS" "IR2F" "NWM" "OSF" "homepage")
+websites=("gregorypropf.com" "implicitpixels.com-dev")
+WEB_ROOT="/var/www"
 
 # for d in "${dirs[@]}"; do
 #   CMD="rsync -avz --delete --partial --progress $HOME/$d ${REMOTE_HOST}:"
@@ -28,20 +30,30 @@ symlinks=("2025" "BUSINESS" "IR2F" "NWM" "OSF" "homepage")
 # done
 
 function sync_files {
+  local root_dir="$1"
+  shift
+  local -a files_to_sync=("${@}")
   #local FILES=$1
-  for d in "${@}"; do
-    CMD="rsync -avz --delete --partial --progress $HOME/$d ${REMOTE_HOST}:"
-    echo "Running: $CMD"
+  echo "------------------------------------------------------------------" | tee -a ${SYNC_LOG}
+  echo "${TIMESTAMP}: Root dir: $root_dir" | tee -a ${SYNC_LOG}
+  echo "${TIMESTAMP}: Files to sync: ${files_to_sync[*]}" | tee -a ${SYNC_LOG}
+  for d in "${files_to_sync[@]}"; do
+    CMD="rsync -avz --delete --partial --progress $root_dir/$d ${REMOTE_HOST}:$root_dir/"
+    TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "${TIMESTAMP}: Running: $CMD"
     $CMD || {
-      echo "Warning: Could not sync '$HOME/$d' to '${REMOTE_HOST}:$d'. It may not exist or you may not have permission." | tee SYNC_HAD_ERRORS
+      echo "Warning: Could not sync '$root_dir/$d' to '${REMOTE_HOST}:$root_dir/$d'. It may not exist or you may not have permission." | tee SYNC_HAD_ERRORS
       exit 1
     }
-    echo "${TIMESTAMP}: Synced: '$HOME/$d' to '${REMOTE_HOST}:$d'" | tee -a ${SYNC_LOG}
+    echo "${TIMESTAMP}: Synced: '$HOME/$d' to '${REMOTE_HOST}:$root_dir/$d'" | tee -a ${SYNC_LOG}
   done
 
 }
 
-sync_files "${files[@]}"
-sync_files "${symlinks[@]}"
+sync_files $HOME "${files[@]}"
+sync_files $HOME "${symlinks[@]}"
+sync_files $WEB_ROOT "${websites[@]}"
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+echo "${TIMESTAMP}: Mirror sync completed successfully!!" | tee -a ${SYNC_LOG}
 
 
